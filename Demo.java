dc7be2d14d250bdb652da05acfddfc615f261fb2
FelixvL <digitaleservice@gmail.com>
2019-03-13 15:46:08 +0100

    galgje commit 1

--- Demo.java
+++ Demo.java
@@ -7,16 +7,32 @@ public class Demo {
 	}
 }
 class Galgje{
-	void starten() {
+	String geheimWoord;
+	Scanner scanner = new Scanner(System.in);
+	void woordInvoeren() {
 		System.out.println("Welkom bij Galgje");
 		System.out.println("Voer een woord in: ");
-		Scanner scanner = new Scanner(System.in);
-		String geheimWoord = scanner.nextLine();
+		geheimWoord = scanner.nextLine();
+	}
+	void radenLetterInWoord() {
 		int lengteWoord = geheimWoord.length();
 		for(int i = 0 ; i < lengteWoord ; i++) {
 			System.out.print("_ ");
 		}			
 		System.out.println("");
+		System.out.println("Voer een letter in:");
+		String geradenLetter = scanner.nextLine();
+		for(int j = 0 ; j < lengteWoord; j++) {
+			if( geheimWoord.charAt(j) == geradenLetter.charAt(0)  ) {
+				System.out.println(true);
+			}else {
+				System.out.println(false);
+			}
+		}
+	}
+	void starten() {
+		woordInvoeren();
+		radenLetterInWoord();		
 	}
 }
 
